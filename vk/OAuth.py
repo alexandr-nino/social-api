ddd9c9fd82350b56ae39f2d4e77783ca62b74f12
Molofeev Alexandr <alexandr.nino@gmail.com>
2013-01-24 01:07:09 +0400

    pep8 + pylint checked

--- vk/OAuth.py
+++ vk/OAuth.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python
 # -*- coding:u8 -*-
+'''
+doc string
+'''
 __author__ = '_killed_'
 
-from PyQt4 import QtWebKit,QtGui,QtCore
+from PyQt4 import QtWebKit, QtGui, QtCore
 
 
 class vk_OAuth(QtGui.QDialog):
-    def __init__(self,auth_page=None,parent=None):
-        super(vk_OAuth,self).__init__(parent)
-        self.resize(640,480)
+    """
+    get access_token from vk.com
+    """
+    def __init__(self, auth_page = None, parent = None):
+        """
+        init QDialog with QWebView
+        """
+        super(vk_OAuth, self).__init__(parent)
+        self.resize(640, 480)
 
         self.web_view = QtWebKit.QWebView(self)
         self.web_view.load(QtCore.QUrl(auth_page))
-        self.connect(self.web_view,QtCore.SIGNAL("loadFinished(bool)"),self.redirectEvent)
+        self.connect(self.web_view,
+                        QtCore.SIGNAL("loadFinished(bool)"),
+                        self.redirectEvent)
 
     def run(self):
+        """
+        auth runner
+        """
         if self.exec_():
-            return True,self.result
+            return True, self.result
         else:
-            return False,self.result
+            return False, self.result
 
-    def redirectEvent(self,hz=None):
+    def redirectEvent(self, QEvent=None):
+        """
+        check redirect page for get response from URI
+        """
         page = str(self.web_view.url().toString())
         if "blank.html#" in page:
-            uri=page.split("#")[-1]
-            uri=uri.split("&")
-            uri=[i.split("=") for i in uri]
-            self.result=dict(uri)
+            uri = page.split("#")[-1]
+            uri = uri.split("&")
+            uri = [i.split("=") for i in uri]
+            self.result = dict(uri)
             if "error" in self.result:
                 self.reject()
             else:
                 self.accept()
 
     def resizeEvent(self, QResizeEvent):
+        """
+               resize QWebView with Dialog window
+                """
         self.web_view.resize(QResizeEvent.size())
--- vk/__init__.py
+++ vk/__init__.py
@@ -1,30 +1,37 @@
+'''
+doc string
+'''
 #!/usr/bin/python
 # -*- coding:u8 -*-
 __author__ = '_killed_'
+import json
+import urllib2
 
-import json,urllib2
+PERMS = {
+    1: None,  # Notification
+    2: 'friends',  # Friends
+    4: None,  # Photos
+    8: None,  # Music
+    16: 'video',  # Videos
+    32: None,  # Apps
+    64: None,  # asks
+    128: None,  # Wiki pages
+    256: None,  # Left menu app link
+    512: None,  # Link for fast comment link to app on walls
+    1024: None,  # Statuses
+    2048: None,  # Notes
+    4096: None,  # more methods for messages
+    # Desktop only
+    8192: None,  # Wall. not for sites
+    32768: None,  # ADS
+    131072: 'docs',  # docs
+    262144: None,  # Groups
+    524288: None,  # notify answers
+    1048576: None,  # Stats Groups and Apps if i'm admin
+}
+AVAILIABLE_PERMS = filter(lambda x: x, PERMS.itervalues())
 
-PERMS={
-    1:None,#" 	Пользователь разрешил отправлять ему уведомления.",
-    2:'friends',# " 	Доступ к друзьям.",
-    4:None,#' 	Доступ к фотографиям.',
-    8:None,#' 	Доступ к аудиозаписям.',
-    16:'video',#' 	Доступ к видеозаписям.',
-    32:None,#' 	Доступ к предложениям.',
-    64:None,#' 	Доступ к вопросам.',
-    128:None,#' 	Доступ к wiki-страницам.',
-    256:None,#' 	Добавление ссылки на приложение в меню слева.',
-    512:None,#' 	Добавление ссылки на приложение для быстрой публикации на стенах пользователей.',
-    1024:None,#' 	Доступ к статусам пользователя.',
-    2048:None,#' 	Доступ заметкам пользователя.',
-    4096:None,#' 	(для Desktop-приложений) Доступ к расширенным методам работы с сообщениями.',
-    8192:None,#' 	Доступ к обычным и расширенным методам работы со стеной.Внимание, данное право доступа недоступно для сайтов (игнорируется при попытке авторизации).',
-    32768:None,#' 	Доступ к функциям для работы с рекламным кабинетом.',
-    131072:'docs',#' 	Доступ к документам пользователя.',
-    262144:None,#' 	Доступ к группам пользователя.',
-    524288:None,#' 	Доступ к оповещениям об ответах пользователю.',
-    1048576:None,#' 	Доступ к статистике групп и приложений пользователя, администратором которых он является. '}
-    }
+
 from vk_friends import friends
 from vk_video import video
 from vk_documents import docs
@@ -34,19 +41,24 @@ class VK(object):
     """
     base class for VK API
     """
+    def __init__(self, app, uid, token):
+        """
 
-    def __init__(self,app,uid,token):
-        self.__app=app
-        self.__token=token
-        self.__uid=uid
-        self.__error={}
-        self.__result={}
+        """
+        self.__app = app
+        self.__token = token
+        self.__uid = uid
+        self.__error = {}
+        self.__result = {}
 
-        if self.handler('getUserSettings',uid=self.__uid):
+        if self.handler('getUserSettings', uid=self.__uid):
             self.__perms = int(self.__result)
             for permission in PERMS.keys():
-                if self.__perms&permission==permission and PERMS[permission]:
-                    setattr(self,PERMS[permission],eval(PERMS[permission]+"(self.handler)"))
+                if (self.__perms & permission == permission)\
+                        and PERMS[permission]:
+                    setattr(self,
+                            PERMS[permission],
+                            eval(PERMS[permission] + "(self.handler)"))
 
     def get_uid(self):
         """
@@ -78,68 +90,45 @@ class VK(object):
                 """
         return self.__perms
 
-
-    def handler(self,method,**kwargs):
+    def handler(self, method, **kwargs):
         """
         global request handler
-        :param method: str;
         """
-        url = self.__makeUrl(method,**kwargs)
-        response=json.loads(urllib2.urlopen(url).read())
+        url = self._make_url(method, **kwargs)
+        response = json.loads(urllib2.urlopen(url).read())
         if "error" in response:
-            self.__error=response
+            self.__error = response
             return False
-        self.__result=response["response"]
+        self.__result = response["response"]
         return True
 
-
-
-    def __makeUrl(self,method,**kwargs):
+    def _make_url(self, method, **kwargs):
         """
         generate request uri
-        :param method: str
         """
-        url="https://api.vk.com/method/{0}?{1}access_token={2}"
-        data=""
+        url = "https://api.vk.com/method/{0}?{1}access_token={2}"
+        data = ""
         for item in kwargs.items():
-            if type(item[1]) in [list,tuple]:
-                data+="{0}={1}&".format( item[0],",".join([str(i) for i in item[1]]) )
+            if type(item[1]) in [list, tuple]:
+                data += "{0}={1}&".format(
+                        item[0],
+                        ",".join([str(i) for i in item[1]]))
             else:
-                data+="{0}={1}&".format( item[0],item[1])
-        return url.format(method,data,self.__token)
+                data += "{0}={1}&".format(item[0], item[1])
+        return url.format(method, data, self.__token)
 
     @staticmethod
-    def GetPopupUrl(app_id=None,scope = [
-                                        "notify",# 	Пользователь разрешил отправлять ему уведомления.
-                                        "friends",# 	Доступ к друзьям.
-                                        "photos",# 	Доступ к фотографиям.
-                                        "audio",# 	Доступ к аудиозаписям.
-                                        "video",# 	Доступ к видеозаписям.
-                                        "docs",# 	Доступ к документам.
-                                        "notes",# 	Доступ заметкам пользователя.
-                                        "pages",# 	Доступ к wiki-страницам.
-                                        "offers",# 	Доступ к предложениям (устаревшие методы).
-                                        "questions",# 	Доступ к вопросам (устаревшие методы).
-                                        "wall",# 	Доступ к обычным и расширенным методам работы со стеной.
-                                        "groups",# 	Доступ к группам пользователя.
-                                        "messages",# 	(для Standalone-приложений) Доступ к расширенным методам работы с сообщениями.
-                                        "notifications",# 	Доступ к оповещениям об ответах пользователю.
-                                        "stats",# 	Доступ к статистике групп и приложений пользователя, администратором которых он является.
-                                        "ads",# 	Доступ к расширенным методам работы с рекламным API.
-                                        "offline"# 	Доступ к API в любое время со стороннего сервера.
-                                    ]):
+    def get_popup_url(app_id, scope=AVAILIABLE_PERMS):
         """
-        :param app_id: int
-        http://vk.com/pages?oid=-1&p=%D0%9F%D1%80%D0%B0%D0%B2%D0%B0_%D0%B4%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%B0_%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9
+        http://vk.com/pages?oid=-1&p=Права_доступа_приложенийu
         """
-        if not app_id:
-            raise ValueError,"API ID is not defined."
-        if not scope or type(scope) not in [tuple,list]:
-            raise ValueError,"Scope must be list or tuple type"
+        assert type(app_id) == int
+        assert type(scope) in [list, tuple]
 
-        url="http://oauth.vk.com/authorize?client_id={0}&scope={1}&redirect_uri=http://oauth.vk.com/blank.html&display=popup&response_type=token"
-        return url.format(app_id,",".join(scope))
-
-
-
-
+        url = "http://oauth.vk.com/authorize?" \
+              "client_id={0}&" \
+              "scope={1}&" \
+              "redirect_uri=http://oauth.vk.com/blank.html&" \
+              "display=popup&" \
+              "response_type=token"
+        return url.format(app_id, ",".join(scope))
